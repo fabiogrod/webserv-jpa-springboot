f248935f00ab9aa0c85e812580cdb25300fa59ca
fabiogrod <fabiogrimello@gmail.com>
2020-11-25 12:43:44 -0300

    Aula 306 - Início do Projeto

---
+++ src/main/java/com/ws/websrvcjpaspringboot/WsWebsrvcJpaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ws.websrvcjpaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsWebsrvcJpaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsWebsrvcJpaSpringbootApplication.class, args);
+	}
+
+}
