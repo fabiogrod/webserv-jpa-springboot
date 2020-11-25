96df78c3e6f4569c4e8d6b66e44febb951e3f909
fabiogrod <fabiogrimello@gmail.com>
2020-11-25 17:39:00 -0300

    Aula 308 - Banco de dados H2, perfil de teste e JPA

--- src/main/java/com/ws/websrvcjpaspringboot/entidades/Usuario.java
+++ src/main/java/com/ws/websrvcjpaspringboot/entidades/Usuario.java
@@ -2,11 +2,16 @@ package com.ws.websrvcjpaspringboot.entidades;
 
 import java.io.Serializable;
 
-public class Usuario implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
 	private String nome;
 	private String email;
 	private String fone;
