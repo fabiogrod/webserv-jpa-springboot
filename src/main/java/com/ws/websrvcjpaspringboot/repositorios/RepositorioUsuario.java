fbb90fa56b985b6079531264b7835966dcfc1eb9
fabiogrod <fabiogrimello@gmail.com>
2020-11-29 17:31:07 -0300

    Aula 334 - Implantação do programa no Heroku

--- src/main/java/com/ws/websrvcjpaspringboot/repositorios/RepositorioUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.ws.websrvcjpaspringboot.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ws.websrvcjpaspringboot.entidades.Usuario;
-
-public interface RepositorioUsuario extends JpaRepository<Usuario, Long>{
-	
-}
